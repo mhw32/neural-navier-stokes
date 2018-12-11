b4da74783a5de18261d44e4d77b8ef0d35b344a8
mhw32 <me@mikewuis.me>
2018-12-11 09:57:12 -0800

    two more to-dos... define ELBO and define training script

---
+++ nlds/lorenz.py
@@ -0,0 +1,28 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import os
+import sys
+import numpy as np
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+from torch.optim import lr_scheduler
+
+from datasets import BernoulliLorenz
+from nlds import RSSNLDS
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('out_dir', type=str, help='where to save outputs')
+    parser.add_argument('--cuda', action='store_true', default=False,
+                        help='enables CUDA training')
+    args = parser.parse_args()
+    args.cuda = args.cuda and torch.cuda.is_available()
+    device = torch.device('cuda' if args.cuda else 'cpu')
