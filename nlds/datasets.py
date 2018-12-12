ca57ac9d01ae9cca65fbb1ed39fd57c3f2c95e4f
mhw32 <me@mikewuis.me>
2018-12-11 18:44:29 -0800

    use datasets to generate stuff

--- nlds/datasets.py
+++ nlds/datasets.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 import torch.utils.data as data
 
 import numpy as np
+from tqdm import tqdm
 
 
 class BernoulliLorenz(data.Dataset):
@@ -50,29 +51,29 @@ def build_bernoulli_lorenz_dataset(N, T, dt=0.01):
                hyperparameter for sampling from Lorenz
     """
     X, Y, C, D = [], [], [], []
-    for n in xrange(N):
+    for n in tqdm(range(N)):
         x_n = sample_lorenz_attractor(dt=dt, stepCnt=T)
-        c_n = np.random.randn((100, 3))
+        c_n = np.random.randn(100, 3)
         d_n = np.random.randn(100)
-        y_n = np.dot(c_n, x_n) + d_n
+        y_n = np.dot(c_n, x_n.T) + d_n[:, np.newaxis]
         X.append(x_n)
         Y.append(y_n)
         C.append(c_n)
         D.append(d_n)
     
-    X = np.vstack(X)
-    Y = np.vstack(Y)
-    C = np.vstack(C)
-    D = np.vstack(D)
+    X = np.rollaxis(np.dstack(X), 2, 0)
+    Y = np.rollaxis(np.dstack(Y), 2, 0)
+    C = np.rollaxis(np.dstack(C), 2, 0)
+    D = np.rollaxis(np.dstack(D), 2, 0)
 
     return X, Y, C, D
 
 
 def sample_lorenz_attractor(dt=0.01, stepCnt=10000):
     # Need one more for the initial values
-    xs = np.empty((stepCnt + 1,))
-    ys = np.empty((stepCnt + 1,))
-    zs = np.empty((stepCnt + 1,))
+    xs = np.empty((stepCnt,))
+    ys = np.empty((stepCnt,))
+    zs = np.empty((stepCnt,))
 
     xs[0], ys[0], zs[0] = (0., 1., 1.05)
 
@@ -84,13 +85,28 @@ def sample_lorenz_attractor(dt=0.01, stepCnt=10000):
         return x_dot, y_dot, z_dot
 
     # Stepping through "time".
-    for i in range(stepCnt):
+    for i in range(stepCnt - 1):
         # Derivatives of the X, Y, Z state
         x_dot, y_dot, z_dot = lorenz(xs[i], ys[i], zs[i])
         xs[i + 1] = xs[i] + (x_dot * dt)
         ys[i + 1] = ys[i] + (y_dot * dt)
         zs[i + 1] = zs[i] + (z_dot * dt)
 
-    data = np.concatenate([xs, ys, zs]).T
+    data = np.stack([xs, ys, zs]).T
     
     return data
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('N', type=int, help='number of data points')
+    parser.add_argument('T', type=int, help='number of timesteps per data point')
+    parser.add_argument('--dt', type=float, default=0.01, help='hyperparameter for dynamics [default: 0.01]')
+    args = parser.parse_args()
+
+    X, Y, C, D = build_bernoulli_lorenz_dataset(args.N, args.T, dt=args.dt)
+    np.save('./data/X.npy', X)
+    np.save('./data/Y.npy', Y)
+    np.save('./data/C.npy', C)
+    np.save('./data/D.npy', D)
