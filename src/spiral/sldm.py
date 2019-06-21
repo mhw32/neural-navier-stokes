d56a79adcccc89cd27c627e8a2d3b288c8635271
mhw32 <me@mikewuis.me>
2019-06-21 01:15:06 -0700

    begin programming ssldm...  we need sparsemax! and implicit reparams

--- src/spiral/sldm.py
+++ src/spiral/sldm.py
@@ -14,3 +14,62 @@ import torch.nn.functional as F
 from src.spiral.dataset import generate_spiral2d
 from src.spiral.utils import AverageMeter, log_normal_pdf, normal_kl
 from src.spiral.ldm import reverse_sequences_torch
+from src.spiral.ldm import LDM, Combiner, Transistor, Emitter
+
+
+class SLDM(nn.Module):
+    """
+    Switching-State Linear Dynamical Model parameterizes by neural networks.
+
+    n_states := integer
+                number of states
+    y_dim := integer
+            number of input dimensions
+    x_dim := integer
+             number of latent dimensions
+    z_dim := integer
+             number of space dimensions
+    x_emission_dim := integer
+                      hidden dimension from y_dim -> x_dim
+    z_emission_dim := integer
+                      hidden dimension from x_dim -> z_dim
+    x_transition_dim := integer
+                        hidden dimension from x_dim -> x_dim
+    z_transition_dim := integer
+                        hidden dimension from z_dim -> z_dim
+    y_rnn_dim := integer
+                 hidden dimension for RNN over y
+    x_rnn_dim := integer
+                 hidden dimension for RNN over x
+    y_rnn_dropout_rate := float [default: 0.]
+                          dropout over nodes in RNN
+    x_rnn_dropout_rate := float [default: 0.]
+                          dropout over nodes in RNN
+    """
+    def __init__(self, n_states, y_dim, x_dim, z_dim,  x_emission_dim, z_emission_dim, 
+                 x_transition_dim, z_transition_dim, y_rnn_dim, x_rnn_dim, 
+                 y_rnn_dropout_rate=0., x_rnn_dropout_rate=0.):
+        super().__init__()
+
+        # Define (trainable) parameters z_0 and z_q_0 that help define
+        # the probability distributions p(z_1) and q(z_1)
+        self.z_0 = nn.Parameter(torch.zeros(z_dim * n_states))
+        self.z_q_0 = nn.Parameter(torch.zeros(z_dim * n_states))
+
+        # Define a (trainable) parameter for the initial hidden state of each RNN
+        self.h_0s = nn.ParameterList([nn.Parameter(torch.zeros(1, 1, self.x_rnn_dim))
+                                      for _ in range(n_states)])
+
+        # RNNs over continuous latent variables, x
+        self.x_rnns = nn.ModuleList([
+            nn.RNN(self.x_dim, self.x_rnn_dim,
+                   nonlinearity='relu', batch_first=True,
+                   dropout=self.x_rnn_dropout_rate)
+            for _ in range(n_states)
+        ])
+
+        self.ldms = nn.ModuleList([
+            LDM(y_dim, x_dim, x_emission_dim, x_transition_dim,
+                y_rnn_dim, rnn_dropout_rate=y_rnn_dropout_rate)
+            for _ in range(n_states)
+        ])
