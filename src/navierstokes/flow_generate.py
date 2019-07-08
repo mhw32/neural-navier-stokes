5ab986d5abcb9893f53c4b5c06fe78bedeb627cf
Mike Wu <me@mikehwu.com>
2019-07-08 12:08:45 -0700

    add code for robust bonudary conditions

--- src/navierstokes/flow_generate.py
+++ src/navierstokes/flow_generate.py
@@ -7,16 +7,33 @@ Optionally, users can optionally add variance in
 a source term as well as add periodic boundary 
 conditions.
 
-TODO: handle Neumann boundary conditions.
+Notes:
+    Dirichlet boundary conditions:
+        https://github.com/barbagroup/CFDPython
+
+    Neumann boundary conditions: 
+        http://folk.ntnu.no/leifh/teaching/tkt4140/._main056.html
+        
+    Periodic boundary conditions:
+        https://nbviewer.jupyter.org/github/barbagroup/CFDPython/blob/master/lessons/15_Step_12.ipynb
 """
 
 import numpy as np
 
 
 class BoundaryCondition():
-    """General class for a boundary condition."""
+    """General class for a boundary condition.
+    
+    Args:
+    -----
+    x := integer 
+         x-coordinate in matrix A
+    y := integer 
+         y-coordinate in matrix A
+    """
 
-    def __init__(self, dirichlet=None, neumann=None, periodic=False):
+    def __init__(self, x, y, min_x, max_x, min_y, max_y, 
+                 dirichlet=None, neumann=None, periodic=False):
         assert (dirichlet not None) or (neumann not None) or (not periodic)
         if dirichlet is not None:
             assert neumann is None and not periodic
@@ -27,53 +44,99 @@ class BoundaryCondition():
         self.dirichlet = dirichlet
         self.neumann = neumann
         self.periodic = periodic
-
-    def get(self):
-        if self.dirichlet:
-            return self.dirichlet, 'dirichlet'
-        elif self.neumann:
-            return self.neumann, 'neumann'
-        else:
-            return self.periodic, 'periodic'
-
-
-class BoundaryConditions():
-    """Specifies boundary conditions for either Momentum 
-    or Pressure (in Navier Stokes).
-
-    You can either specify a dirichlet, neumann, or periodic
-    condition at each boundary. We will not allow multiple 
-    types of boundaries.
-
-    Args:
-    -----
-    x0 := BoundaryCondition object
-          boundary condition at x=0
-    y0 := BoundaryCondition object
-          boundary condition at y=0
-    xn := BoundaryCondition object
-          boundary condition at x=n
-    yn := BoundaryCondition object 
-          boundary condition at y=n
-    """
-    def __init__(self, x0_bc, y0_bc, xn_bc, yn_bc):
-        self.x0_bc = x0_bc
-        self.y0_bc = y0_bc
-        self.xn_bc = xn_bc
-        self.yn_bc = yn_bc
+        self.x, self.x_min, self.x_max = x, x_min, x_max
+        self.y, self.y_min, self.y_may = y, y_min, y_max
 
     def is_dirichlet(self):
-        pass
-    
+        return self.dirichlet is not None
+
     def is_neumann(self):
-        pass
-    
+        return self.neumann is not None
+
     def is_periodic(self):
+        return self.periodic
+
+    def is_left_x_boundary(self):
+        return self.x == self.min_x
+    
+    def is_right_x_boundary(self):
+        return self.x == self.max_x
+    
+    def is_left_y_boundary(self):
+        return self.y == self.min_y
+    
+    def is_right_y_boundary(self):
+        return self.y == self.max_y
+
+    def apply(self):
+        raise NotImplementedError
 
 
-class PressureBoundaryConditions(BoundaryConditions):
+
+class UMomentumBoundaryCondition(BoundaryCondition):
+    
+    def _apply_dirichlet(self, u):
+        """
+        Dirichlet BCs are easy, all we have to do is set 
+        the point to a particular value.
+        """
+        u[self.x, self.y] = self.dirichlet
+
+    def _apply_neumann(self, u, dunit):
+        """
+        Assume that we now du/dx at some coordinate (i,j). Then, 
+        we can say as a simple forward difference estimate.
+            
+            du/dx|(i,j) = (-3*u[i,j] + 4u[i+1,j] - u[i+2,j]) / 2*dx
+            du/dy|(i,j) = (-3*u[i,j] + 4u[i,j+1] - u[i,j+2]) / 2*dy
+        
+        For end points on the other edge, we would use a backward 
+        difference estimate.
+            
+            du/dx|(i,j) = (3*u[i,j] - 4u[i-1,j] + u[i-2,j]) / 2*dx
+            du/dy|(i,j) = (3*u[i,j] - 4u[i,j-1] + u[i,j-2]) / 2*dy
+        
+        More tricks exist for central difference that include making
+        ghost mesh points but too complicated.
+
+        Args:
+        -----
+        u := np.array
+             momentum tensor
+        dunit := discrete interval along x/y direction
+        """
+        if self.is_left_x_boundary():
+            dudx, dx = self.neumann, dunit
+            u[self.x, self.y] = (4./3. * u[self.x+1,self.y] - 
+                                 1./3 * u[self.x+2,self.y] - 
+                                 2./3. * dx  * dudx)
+        elif self.is_right_x_boundary():
+            dudx, dx = self.neumann, dunit
+            u[self.x, self.y] = (4./3. * u[self.x-1,self.y] - 
+                                 1./3 * u[self.x-2,self.y] + 
+                                 2./3. * dx  * dudx)
+        elif self.is_left_y_boundary():
+            dudy, dy = self.neumann, dunit
+            u[self.x, self.y] = (4./3. * u[self.x,self.y+1] - 
+                                 1./3 * u[self.x,self.y+2] - 
+                                 2./3. * dy  * dudy)
+        elif self.is_right_y_boundary():
+            dudy, dy = self.neumann, dunit
+            u[self.x, self.y] = (4./3. * u[self.x,self.y-1] - 
+                                 1./3 * u[self.x,self.y-2] + 
+                                 2./3. * dy  * dudy)
+        else:
+            raise Exception('invalid point not on boundary.')
+
+    def _apply_periodic(self):
+        raise NotImplementedError
+
+
+class VMomentumBoundaryCondition(UMomentumBoundaryCondition):
+    # these will become very different once we start to 
+    # think about periodic conditions.
     pass
 
 
-class MomentumBoundaryConditions(BoundaryConditions):
+class PressureBoundaryCondition(UMomentumBoundaryCondition):
     pass
