db55640ef96c81f973b0014ca1d968385fbff441
mhw32 <me@mikewuis.me>
2019-08-11 15:51:58 -0700

    okay done with chorins + finite diff

--- src/chorin_fd/simulate.py
+++ src/chorin_fd/simulate.py
@@ -22,6 +22,8 @@ class NavierStokesSystem():
            list of BoundaryCondition objects
     nt : integer
          number of time steps to run
+    nit : integer
+          number of internal iterations for convergence in pressure estimate
     nx : integer
          number of grid points across x
     ny : integer
@@ -32,18 +34,21 @@ class NavierStokesSystem():
           constant in the Navier Stokes equations
     nu : float
          constant in the Navier Stokes equations
+    beta : float
+           constant in successive over-relaxation
     """
 
     def __init__(self, u_ic, v_ic, p_ic, u_bc, v_bc, p_bc, 
-                 nt=200, nx=50, ny=50, dt=0.001, rho=1, nu=1):
+                 nt=200, nit=50, nx=50, ny=50, dt=0.001, 
+                 rho=1, nu=1, beta=1.25):
         self.u_ic, self.v_ic, self.p_ic = u_ic, v_ic, p_ic
         self.u_bc, self.v_bc, self.p_bc = u_bc, v_bc, p_bc
-        self.nt, self.dt, self.nx, self.ny = nt, dt, nx, ny
+        self.nt, self.nit, self.dt, self.nx, self.ny = nt, nit, dt, nx, ny
         # hard code to size of x over 2 (un-dimensionalize to [-1, 1])
         self.dx, self.dy = 2. / (self.nx - 1), 2. / (self.ny - 1)
-        self.rho, self.nu = rho, nu
+        self.rho, self.nu, self.beta = rho, nu, beta
 
-    def _get_intermediate_velocity_field(self, u, v, u1, v1):
+    def _predictor_step(self, u, v, u1, v1):
         dt, dx, dy = self.dt, self.dx, self.dy
         nu = self.nu
 
@@ -71,13 +76,6 @@ class NavierStokesSystem():
                                                     -1/2. * ((vn1[2:, 1:-1] - 2 * vn1[1:-1, 1:-1] + vn1[:-2, 1:-1]) / dx**2 + 
                                                              (vn1[1:-1, 2:] - 2 * vn1[1:-1, 1:-1] + vn1[1:-1, :-2]) / dy**2))
 
-        # set boundary conditions
-        for bc in self.u_bc:
-            ui = bc.apply(ui)
-        
-        for bc in self.v_bc:
-            vi = bc.apply(vi)
-
         return ui, vi
 
     def _get_pressure(self, ui, vi, p):
@@ -86,18 +84,60 @@ class NavierStokesSystem():
         https://www3.nd.edu/~gtryggva/CFD-Course2010/2010-Lecture-11.pdf
 
         We need a separate process to solve
-
-        laplace(p) = rho / dt * divergence(u*)
-        """
-        dt, dx, dy = self.dt, self.dx, self.dy
-        rho = self.rho
-
+            laplace(p) = rho / dt * divergence(u*)
         
+        Use successive over-relaxation to solve this elliptic eqn.
+        """
+        nx, ny = self.nx, self.ny
+        dt, dx, dy = self.dt, self.dx, self.dy
+        rho, beta = self.rho, self.beta
 
-        pn1 = p.copy()  # p^{n+1}
+        tol, err, it = 5e-6, 1, 1
+        pPrev = p.copy()
+
+        C = np.zeros_like(ui)
+        C[1:-1, 1:-1] = rho / dt * ((ui[2:, :] - ui[:-2, :]) / self.dx +
+                                    (ui[:, 2:] - ui[:, :-2]) / self.dy)
+
+        while ((err > tol) and (it < self.nit)):
+            for i in range(1, nx):
+                for j in range(1, ny):
+                    p[i, j] =  (beta * 1/4. * ((p[i+1, j] + p[i-1, j]) / dx**2 + 
+                                               (p[i, j+1] + p[i, j-1]) / dy**2 -
+                                               C[i, j]) + 
+                                (1 - beta) * p[i, j])
+
+            err = np.max(np.abs(p - pPrev)) 
+            pPrev = p.copy()
+            it = it + 1
+
+        return p
+
+    def _correction_step(self, ui, vi, p):
+        dt, dx, dy = self.dt, self.dx, self.dy
+        un1 = ui - dt / dx * (p[2:, :] - p[:-2, :])
+        vn1 = vi - dt / dy * (p[:, 2:] - p[:, :-2])
+        
+        return un1, vn1
 
     def step(self, un, vn, un1, vn1, p):
-        pass
+        ui, vi = self._predictor_step(un, vn, un1, vn1)
+
+        # set boundary conditions
+        for bc in self.u_bc:
+            ui = bc.apply(ui)
+        
+        for bc in self.v_bc:
+            vi = bc.apply(vi)
+
+        p = self._get_pressure(ui, vi, p)
+
+        # apply neumann boundary conditions
+        for bc in self.p_bc:
+            p = bc.apply(p)
+
+        un1, vn1 = self._correction_step(ui, vi, p)
+        return un1, vn1
 
     def simulate(self):
         # collect propagations for dataset
