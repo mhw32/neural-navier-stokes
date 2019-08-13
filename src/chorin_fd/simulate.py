0a6a0b8631b7b7706cb47b2187c86491a19c48fd
Mike Wu <mhw32@users.noreply.github.com>
2019-08-12 18:44:41 -0700

    Implement (Implicit) Crank-Nicholson for Time Discretization (#8)
    Woot! now we have pretty much a state-of-the-art finnite difference simulator. This will be improtant when developing spectral methods to have something good to compare against. Still need to test before wrapping this up

--- src/chorin_fd/simulate.py
+++ src/chorin_fd/simulate.py
@@ -1,11 +1,12 @@
 import numpy as np
+from scipy.sparse import diags
 from tqdm import tqdm
 
 
 class NavierStokesSystem():
     """
     Wrapper class around a 2D Incompressible Navier Stokes system.
-    
+
     Args:
     -----
     u_ic : np.array
@@ -36,19 +37,26 @@ class NavierStokesSystem():
          constant in the Navier Stokes equations
     beta : float
            constant in successive over-relaxation
+    method : string
+             explicit | semi_implicit
+             explicit will use Adam-Bashford for advection and diffusion terms
+             semi_implicit will use Adam-Bashform for advection
+                and Crank-Nicholson for diffusion terms
     """
 
-    def __init__(self, u_ic, v_ic, p_ic, u_bc, v_bc, p_bc, 
-                 nt=200, nit=50, nx=50, ny=50, dt=0.001, 
-                 rho=1, nu=1, beta=1.25):
+    def __init__(self, u_ic, v_ic, p_ic, u_bc, v_bc, p_bc,
+                 nt=200, nit=50, nx=50, ny=50, dt=0.001,
+                 rho=1, nu=1, beta=1.25, method='semi_implicit'):
         self.u_ic, self.v_ic, self.p_ic = u_ic, v_ic, p_ic
         self.u_bc, self.v_bc, self.p_bc = u_bc, v_bc, p_bc
         self.nt, self.nit, self.dt, self.nx, self.ny = nt, nit, dt, nx, ny
         # hard code to size of x over 2 (un-dimensionalize to [-1, 1])
         self.dx, self.dy = 2. / (self.nx - 1), 2. / (self.ny - 1)
         self.rho, self.nu, self.beta = rho, nu, beta
+        assert method in ['semi_implicit', 'explict']
+        self.method = method
 
-    def _predictor_step(self, u, v, u1, v1):
+    def _explicit_predictor_step(self, u, v, u1, v1):
         dt, dx, dy = self.dt, self.dx, self.dy
         nu = self.nu
 
@@ -58,23 +66,99 @@ class NavierStokesSystem():
         un1, vn1 = u1.copy(), v1.copy()  # u^{n-1}, v^{n-1}
 
         # Adam-Bashford for explicit momentum computation
-        ui[1:-1, 1:-1] = un[1:-1, 1:-1] - dt * (3/2. * (un[1:-1, 1:-1] * (un[2:, 1:-1] - un[:-2, 1:-1]) / dx + 
+        ui[1:-1, 1:-1] = un[1:-1, 1:-1] - dt * (3/2. * (un[1:-1, 1:-1] * (un[2:, 1:-1] - un[:-2, 1:-1]) / dx +
                                                         vn[1:-1, 1:-1] * (un[2:, 1:-1] - un[:-2, 1:-1]) / dy)
-                                               -1/2. * (un1[1:-1, 1:-1] * (un1[2:, 1:-1] - un1[:-2, 1:-1]) / dx + 
+                                               -1/2. * (un1[1:-1, 1:-1] * (un1[2:, 1:-1] - un1[:-2, 1:-1]) / dx +
                                                         vn1[1:-1, 1:-1] * (un1[2:, 1:-1] - un1[:-2, 1:-1]) / dy))
-                                        + dt * nu * (3/2. * ((un[2:, 1:-1] - 2 * un[1:-1, 1:-1] + un[:-2, 1:-1]) / dx**2 + 
+                                        + dt * nu * (3/2. * ((un[2:, 1:-1] - 2 * un[1:-1, 1:-1] + un[:-2, 1:-1]) / dx**2 +
                                                              (un[1:-1, 2:] - 2 * un[1:-1, 1:-1] + un[1:-1, :-2]) / dy**2)
-                                                    -1/2. * ((un1[2:, 1:-1] - 2 * un1[1:-1, 1:-1] + un1[:-2, 1:-1]) / dx**2 + 
+                                                    -1/2. * ((un1[2:, 1:-1] - 2 * un1[1:-1, 1:-1] + un1[:-2, 1:-1]) / dx**2 +
                                                              (un1[1:-1, 2:] - 2 * un1[1:-1, 1:-1] + un1[1:-1, :-2]) / dy**2))
 
-        vi[1:-1, 1:-1] = vn[1:-1, 1:-1] - dt * (3/2. * (un[1:-1, 1:-1] * (vn[2:, 1:-1] - vn[:-2, 1:-1]) / dx + 
+        vi[1:-1, 1:-1] = vn[1:-1, 1:-1] - dt * (3/2. * (un[1:-1, 1:-1] * (vn[2:, 1:-1] - vn[:-2, 1:-1]) / dx +
                                                         vn[1:-1, 1:-1] * (vn[2:, 1:-1] - vn[:-2, 1:-1]) / dy)
-                                               -1/2. * (un1[1:-1, 1:-1] * (vn1[2:, 1:-1] - vn1[:-2, 1:-1]) / dx + 
+                                               -1/2. * (un1[1:-1, 1:-1] * (vn1[2:, 1:-1] - vn1[:-2, 1:-1]) / dx +
                                                         vn1[1:-1, 1:-1] * (vn1[2:, 1:-1] - vn1[:-2, 1:-1]) / dy))
-                                        + dt * nu * (3/2. * ((vn[2:, 1:-1] - 2 * vn[1:-1, 1:-1] + vn[:-2, 1:-1]) / dx**2 + 
+                                        + dt * nu * (3/2. * ((vn[2:, 1:-1] - 2 * vn[1:-1, 1:-1] + vn[:-2, 1:-1]) / dx**2 +
                                                              (vn[1:-1, 2:] - 2 * vn[1:-1, 1:-1] + vn[1:-1, :-2]) / dy**2)
-                                                    -1/2. * ((vn1[2:, 1:-1] - 2 * vn1[1:-1, 1:-1] + vn1[:-2, 1:-1]) / dx**2 + 
+                                                    -1/2. * ((vn1[2:, 1:-1] - 2 * vn1[1:-1, 1:-1] + vn1[:-2, 1:-1]) / dx**2 +
                                                              (vn1[1:-1, 2:] - 2 * vn1[1:-1, 1:-1] + vn1[1:-1, :-2]) / dy**2))
+
+        return ui, vi
+
+    def _semi_implicit_predictor_step(self, u, v, u1, v1):
+        dt, dx, dy = self.dt, self.dx, self.dy
+        nu = self.nu
+
+        # important to make copies
+        ut, vt = u.copy(), v.copy()
+        ui, vi = u.copy(), v.copy()  # intermediate fields
+        un, vn = u.copy(), v.copy()
+        un1, vn1 = u1.copy(), v1.copy()  # u^{n-1}, v^{n-1}
+
+        # -- step 0 of crank-nicholson: constants
+
+        A = diags(
+            np.array([
+                np.ones(self.nx-2) * -dt,
+                np.ones(self.nx-2) * (2 * dx**2 + 2 * dt),
+                np.ones(self.nx-2) * -dt,
+            ]),
+            [-1, 0, 1],
+        ).toarray()
+
+        B = diags(
+            np.array([
+                np.ones(self.ny-2) * -dt,
+                np.ones(self.ny-2) * (2 * dy**2 + 2 * dt),
+                np.ones(self.ny-2) * -dt,
+            ]),
+            [-1, 0, 1],
+        ).toarray()
+
+        # -- step 1 of crank-nicholson: u-momentum --
+
+        # adams-bashford estimate for advection terms
+        uHn  = (un[1:-1, 1:-1] * (un[2:, 1:-1] - un[:-2, 1:-1]) / dx +
+                vn[1:-1, 1:-1] * (un[2:, 1:-1] - un[:-2, 1:-1]) / dy)
+        uHn1 = (un1[1:-1, 1:-1] * (un1[2:, 1:-1] - un1[:-2, 1:-1]) / dx +
+                vn1[1:-1, 1:-1] * (un1[2:, 1:-1] - un1[:-2, 1:-1]) / dy)
+        # build C vector
+        uC1 = dt / 2. * (3 * uHn - uHn1)
+        uC2 = dt * nu * ((un[2:, 1:-1] - 2 * un[1:-1, 1:-1] + un[:-2, 1:-1]) / dx**2 +
+                         (un[1:-1, 2:] - 2 * un[1:-1, 1:-1] + un[1:-1, :-2]) / dy**2)
+        uC  = 2 * dx**2 * (uC1 + uC2)
+
+        # solve linear system
+        ut[1:-1, 1:-1] = np.linalg.solve(A, uC)
+
+        # -- step 1 of crank-nicholson: v-momentum --
+
+        # adams-bashford estimate for advection terms
+        vHn  = (un[1:-1, 1:-1] * (vn[2:, 1:-1] - vn[:-2, 1:-1]) / dx +
+                vn[1:-1, 1:-1] * (vn[2:, 1:-1] - vn[:-2, 1:-1]) / dy)
+        uHn1 = (un1[1:-1, 1:-1] * (vn1[2:, 1:-1] - vn1[:-2, 1:-1]) / dx +
+                vn1[1:-1, 1:-1] * (vn1[2:, 1:-1] - vn1[:-2, 1:-1]) / dy)
+        # build C vector
+        vC1 = dt / 2. * (3 * vHn - vHn1)
+        vC2 = dt * nu * ((vn[2:, 1:-1] - 2 * vn[1:-1, 1:-1] + vn[:-2, 1:-1]) / dx**2 +
+                         (vn[1:-1, 2:] - 2 * vn[1:-1, 1:-1] + vn[1:-1, :-2]) / dy**2)
+        vC  = 2 * dx**2 * (vC1 + vC2)
+
+        # solve linear system
+        vt[1:-1, 1:-1] = np.linalg.solve(A, vC)
+
+        # -- step 2 of crank-nicholson: u-momentum --
+
+        uS = (2 * dy**2 * (ut[1:-1, 1:-1] + un[1:-1, 1:-1]) -
+                dt * (un[1:-1, 2:] - 2 * un[1:-1, 1:-1] + un[1:-1, :-2]))
+        ui[1:-1, 1:-1] = np.linalg.solve(B, uS)
+
+        # -- step 2 of crank-nicholson: v-momentum --
+
+        vS = (2 * dy**2 * (vt[1:-1, 1:-1] + vn[1:-1, 1:-1]) -
+                dt * (vn[1:-1, 2:] - 2 * vn[1:-1, 1:-1] + vn[1:-1, :-2]))
+        vi[1:-1, 1:-1] = np.linalg.solve(B, vS)
 
         return ui, vi
 
@@ -85,7 +169,7 @@ class NavierStokesSystem():
 
         We need a separate process to solve
             laplace(p) = rho / dt * divergence(u*)
-        
+
         Use successive over-relaxation to solve this elliptic eqn.
         """
         nx, ny = self.nx, self.ny
@@ -102,12 +186,12 @@ class NavierStokesSystem():
         while ((err > tol) and (it < self.nit)):
             for i in range(1, nx):
                 for j in range(1, ny):
-                    p[i, j] =  (beta * 1/4. * ((p[i+1, j] + p[i-1, j]) / dx**2 + 
+                    p[i, j] =  (beta * 1/4. * ((p[i+1, j] + p[i-1, j]) / dx**2 +
                                                (p[i, j+1] + p[i, j-1]) / dy**2 -
-                                               C[i, j]) + 
+                                               C[i, j]) +
                                 (1 - beta) * p[i, j])
 
-            err = np.max(np.abs(p - pPrev)) 
+            err = np.max(np.abs(p - pPrev))
             pPrev = p.copy()
             it = it + 1
 
@@ -117,7 +201,7 @@ class NavierStokesSystem():
         dt, dx, dy = self.dt, self.dx, self.dy
         un1 = ui - dt / dx * (p[2:, :] - p[:-2, :])
         vn1 = vi - dt / dy * (p[:, 2:] - p[:, :-2])
-        
+
         return un1, vn1
 
     def step(self, un, vn, un1, vn1, p):
@@ -126,7 +210,7 @@ class NavierStokesSystem():
         # set boundary conditions
         for bc in self.u_bc:
             ui = bc.apply(ui)
-        
+
         for bc in self.v_bc:
             vi = bc.apply(vi)
 
@@ -142,7 +226,7 @@ class NavierStokesSystem():
     def simulate(self):
         # collect propagations for dataset
         u_list, v_list, p_list = [], [], []
-        
+
         u, v, p = self.u_ic, self.v_ic, self.p_ic
         u1, v1 = self.u_ic.copy(), self.v_ic.copy()
 
@@ -151,7 +235,7 @@ class NavierStokesSystem():
             u_list.append(u.copy())
             v_list.append(v.copy())
             p_list.append(p.copy())
-    
+
         u_list = np.stack(u_list)
         v_list = np.stack(v_list)
         p_list = np.stack(p_list)
@@ -160,7 +244,7 @@ class NavierStokesSystem():
 
 
 if __name__ == "__main__":
-    from src.boundary import (DirichletBoundaryCondition, 
+    from src.boundary import (DirichletBoundaryCondition,
                               NeumannBoundaryCondition)
 
     nt  = 200
@@ -199,8 +283,8 @@ if __name__ == "__main__":
     ]
 
     system = NavierStokesSystem(
-        u_ic, v_ic, p_ic, u_bc, v_bc, p_bc, 
-        nt=nt, nit=nit, nx=nx, ny=ny, dt=dt, 
+        u_ic, v_ic, p_ic, u_bc, v_bc, p_bc,
+        nt=nt, nit=nit, nx=nx, ny=ny, dt=dt,
         rho=rho, nu=nu,
     )
 
