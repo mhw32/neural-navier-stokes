7845d30f724ac82d579d07f3eb6749769df99398
mhw32 <me@mikewuis.me>
2019-08-25 14:59:20 -0700

    Programmed a few of the important matrices

--- src/chorin_spectral/simulate.py
+++ src/chorin_spectral/simulate.py
@@ -83,6 +83,129 @@ class NavierStokesSystem():
         """
         pass
 
+    # --- begin section on pseudospectral method helpers
+
+    def _get_c_k(self, k):
+        assert k >= 0
+        return 2 if k == 0 else 1
+    
+    def _get_bar_c_k(self, k, N):
+        assert k >= 0
+        return 2 if (k == 0 or k == N) else 1
+
+    def _get_gauss_lobatto_points(self, N, k=1):
+        # N => number of points
+        i = np.arange(N + 1)
+        x_i = np.cos(k * np.pi * i / float(N))
+        return x_i
+
+    def _get_T_matrix(self, N):
+        """
+        Matrix to convert back and forth between spectral coefficients, 
+        \hat{u}_k, and the values at the collocation points, u_N(x_i).
+        This is just a matrix multiplication.
+
+        \mathcal{T} = [\cos k\pi i / N], k,i = 0, ..., N
+        \mathcal{U} = \mathcal{T}\hat{\mathcal{U}}
+
+        where \mathcal{U} = [u(x_0), ..., u(x_N)], the values of the 
+        function at the coordinate points.
+        """
+        T = [
+            self._get_gauss_lobatto_points(N, k=k)
+            for k in np.arange(0, N + 1)
+        ]
+        # N(k) x N(i) since this will be multiplied by the matrix
+        # of spectral coefficients (k)
+        return np.stack(T)
+
+    def _get_inv_T_matrix(self, N):
+        """
+        \mathcal{T}^{-1} = [2(\cos \pi i / N)/(\bar{c}_k \bar{c}_i N)]
+        \hat{\mathcal{U}} = \mathcal{T}\mathcal{U}
+
+        where \hat{\mathcal{U}} = [\hat{u}_0, ..., \hat{u}_N], the 
+        coefficients of the truncated spectral approximation.
+        """
+        inv_T = np.stack([  self._get_gauss_lobatto_points(N, k=1)
+                            for k in np.arange(0, N + 1)  ])
+        inv_T = inv_T.T  # size N(i) x N(k)
+        
+        # bar_c_i is size N(i) x N(k)
+        bar_c_i = np.stack([np.repeat(self._get_bar_c_k(i, N), N + 1)
+                            for i in np.arange(0, N + 1)])
+        bar_c_k = bar_c_i.T
+
+        inv_T = 2 * inv_T / (bar_c_k * bar_c_i * N)
+        # N(i) x N(k) since this will be multiplied by the matrix
+        # of coordinate values
+        return inv_T
+
+    def _get_D_matrix(self, N):
+        """
+        Matrix to compute derivative of coordinate values.
+            \mathcal{D} = [d^{(1)}_{i,j}] for i,j=0...N
+        where if 0 <= i,j <= N, i != j
+            d^{(1)}_{i,j} = \frac{\bar{c}_i}{\bar{c}_j} \frac{(-1)^{i+j}}{(x_i - x_j}
+        else if i <= i <= N - 1, i == j
+            d^{(1)}_{i,i} = -\frac{x_i}{2(1 - x_i^2)}
+        else
+            d^{(1)}_{0,0} = -d^{(1)}_{N, N}
+                          = \frac{2N^2 + 1}{6}
+
+        For numerical stability:
+            let x_i - x_j = 2\sin(\frac{(j+i)\pi}{2N})\sin(\frac{(j-i)\pi}{2N})
+            and 1 - x_i^2 = \sin^2(i\pi/N)
+
+        To ensure the matrix sums to a constant:
+            d^{(1)}_{i,i} = -\sum_{j=0,j!=i}^N d^{(1)}_{i,j}, i=0...N
+
+        This will be used such that 
+            \mathcal{U}^{(1)} = \mathcal{D}\mathcal{U}
+        """
+        # kind of slow to loop but we only ever have to do this once
+        D = np.zeros((N + 1, N + 1))
+        for i in range(0, N + 1):
+            for j in range(0, N + 1):
+                if i != j:
+                    bar_c_i = self._get_bar_c_k(i, N)
+                    bar_c_j = self._get_bar_c_k(j, N)
+                    diff = 2 * np.sin((j + i) * np.pi / (2 * N)) * \
+                            np.sin((j - i) * np.pi / (2 * N))
+                    D[i, j] = bar_c_i / bar_c_j * (-1)**(i + j) / diff
+
+        # now we fill out the diagonals
+        for i in range(0, N + 1):
+            # we can include when i == j in the sum bc its 0
+            D[i, i] = -np.sum(D[i, :])
+        
+        return D
+
+    def _get_D_sqr_matrix(self, N):
+        """
+        A second matrix to compute second derivatives of 
+        coordinate values. In practice, we can just square 
+        the first derivative matrix and apply some fixes.
+
+        This will be used such that 
+            \mathcal{U}^{(2)} = \mathcal{D}^2\mathcal{U}
+        """
+        D = self._get_D_matrix(N)
+        D_sqr_tmp = D**2
+        D_sqr = np.zeros_like(D_sqr_tmp)
+
+        for i in range(0, N + 1):
+            for j in range(0, N + 1):
+                D_sqr[i, j] = D_sqr_tmp[i, j]
+        
+        for i in range(0, N + 1):
+            # we can include when i == j in the sum bc its 0
+            D_sqr[i, i] = -np.sum(D_sqr[i, :])
+        
+        return D_sqr
+
+    # --- end section on pseudospectral method helpers
+
     def _init_variables(self):
         u, v, p = self.u_ic, self.v_ic, self.p_ic
         u, v, p = u.copy(), v.copy(), p.copy()
