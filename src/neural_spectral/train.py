eb6a861d032ffef02dde3630ccb63f1150e5ccbc
mhw32 <me@mikewuis.me>
2019-09-12 12:11:24 -0700

    updates to functions

--- src/neural_spectral/train.py
+++ src/neural_spectral/train.py
@@ -164,6 +164,22 @@ def get_inv_T_matrix(N):
     return torch.from_numpy(inv_T)
 
 
+def log_normal_pdf(x, mean, logvar):
+    const = torch.from_numpy(np.array([2. * np.pi])).float().to(x.device)
+    const = torch.log(const)
+    return -.5 * (const + logvar + (x - mean) ** 2. / torch.exp(logvar))
+
+
+def normal_kl(mu1, lv1, mu2, lv2):
+    v1 = torch.exp(lv1)
+    v2 = torch.exp(lv2)
+    lstd1 = lv1 / 2.
+    lstd2 = lv2 / 2.
+
+    kl = lstd2 - lstd1 + ((v1 + (mu1 - mu2) ** 2.) / (2. * v2)) - .5
+    return kl
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('npz_path', type=str, help='where dataset is stored')
